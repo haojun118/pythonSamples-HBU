42fddaf6f09e723574fd2ad546e21c2c7c0bacdb
xshg <xshg@msn.com>
2017-02-14 22:12:15 +0800

    add drawsquaretocircle.py and update README.md

--- README.md
+++ README.md
@@ -10,3 +10,6 @@ recursion.py	--	递归算法，以阶乘为例
 
 hanoi.py	--	递归算法，汉诺塔
 
+drawsquare.py	--	使用turtle库，示例如何绘制一个矩形
+
+drawsquaretocircle.py	--	使用turtle库，循环回执矩形，行程一个圆形
---
+++ drawsquaretocircle.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# Date: 2017-2-14
+# Ver: 1.0
+# Author: xshg
+# 使用turtle库，绘制一个个矩形，行程一个圆形
+
+import turtle
+
+def draw_square(obj_turtle):
+    for i in range(4):
+        obj_turtle.forward(100)
+        obj_turtle.right(90)
+
+def do_draw():
+    window = turtle.Screen()
+    window.bgcolor("red")
+
+    brad = turtle.Turtle()
+    brad.shape("turtle")
+    brad.color("yellow")
+    
+    for i in range(36):
+        draw_square(brad)
+        brad.right(10)
+
+    window.exitonclick()
+
+do_draw()
